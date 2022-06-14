1255c21e0ceeea0ce2d60dd5b6890c2a1f7889ee
Rohzen <rohzen1@gmail.com>
2022-06-14 17:55:32 +0200

    Description update

--- __manifest__.py
+++ __manifest__.py
@@ -16,8 +16,15 @@ Direct Query Tool for Odoo
 Useful to make db operations:Use at your own risk.
 
 Add a Query function under technical settings, it allows to run query directly on current Odoo database. 
-It allows also to save some query and create unlimited query templates. Use it at your own risk!
+It allows also to save some query and create unlimited query templates. 
 
+It supports also multiple query separated by ;
+Example:
+DELETE FROM fob_order;
+DELETE FROM fob_po_order;
+DELETE FROM account_invoice;
+
+Use it at your own risk!
 Roberto
 www.robertozanardo.com
 
