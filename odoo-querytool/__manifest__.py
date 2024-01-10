83630ec0a6ee4bdba4727b9ffe8b8a673b31056e
Rohzen <rohzen1@gmail.com>
2024-01-10 11:14:55 +0100

    [UPD] Test 14.0

--- odoo-querytool/__manifest__.py
+++ odoo-querytool/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     'name': 'Query Tool',
-    'version': '12.0.0.1.1',
-    'category': 'Sale',
+    'version': '14.0.0.1.1',
+    'category': 'Utilities',
     'summary': 'Query Tool',
     'author': 'Roberto Zanardo',
     'website': 'https://www.robertozanardo.com/',
